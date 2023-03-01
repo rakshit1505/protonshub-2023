973c4f8241fb383ce7ba763f9bcb71d72856831f
Ujjwal Patidar <ujjwal.patidar@protonshub.in>
2023-03-01 17:09:27 +0530

    updated blog pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -79,5 +79,39 @@ class HomeController < ApplicationController
 
   def ror; end
 
-  
+  def web_development_trends_of_2023; end
+
+  def why_you_should_hire_a_dedicated_remote_developer_in_2023; end
+
+  def how_to_find_the_right_mvp_developer_for_your_startup_idea; end
+
+  def how_technology_is_transforming_the_logistics_business; end
+
+  def healthcare_mobile_app_revenue_tips_8_ways_to_grow_your_business; end
+
+  def impact_of_technology_and_automation_on_education; end
+
+  def top_mobile_app_ideas_for_restaurant_and_food_industry; end
+
+  def how_agile_technology_benefits_software_development; end
+
+  def tools_a_react_developer_should_know_in_2022; end
+
+  def the_ultimate_guide_to_mobile_application_development_2022; end
+
+  def trending_ui_ux_design; end
+
+  def multi_tenant_architecture; end
+
+  def hire_remote_developer; end
+
+  def micro_service; end
+
+  def content_system; end
+
+  def react_mobile_app; end
+
+  def global_staffing; end
+
+  def outsourcing; end
 end
